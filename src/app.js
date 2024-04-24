cbb5e56e7963667486e178bdd729c085a9da7aa9
tharuntadisetty04 <tharun5343@gmail.com>
2024-04-24 10:06:07 +0530

    set middlewares

--- src/app.js
+++ src/app.js
@@ -4,13 +4,23 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}))
+
+app.use(express.json({ limit: "16kb" }))
+
+app.use(express.urlencoded({ limit: "16kb", extended: true }))
+
+app.use(express.static("public"))
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
 app.on("error", (err) => {
     console.log("App error : ", err);
     throw err;
-})
-
-app.get('/', (req, res) => {
-    res.send('Hello Worladd')
 })
 
 export default app
