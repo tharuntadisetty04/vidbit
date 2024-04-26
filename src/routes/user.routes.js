aaf9c1a2533457da8dbb5ce5aa86449674b87026
tharuntadisetty04 <tharun5343@gmail.com>
2024-04-26 16:19:59 +0530

    minor bugs fixed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,17 +1,17 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-import upload from '../middlewares/multer.middleware.js'
+import { upload } from '../middlewares/multer.middleware.js'
 
 const router = Router()
 
 router.route("/register").post(
     upload.fields([
         {
-            name: avatar,
+            name: "avatar",
             maxCount: 1
         },
         {
-            name: coverImage,
+            name: "coverImage",
             maxCount: 1
         }
     ]),
